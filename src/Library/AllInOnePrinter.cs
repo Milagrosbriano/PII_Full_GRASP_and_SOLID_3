a015f993688ae25772192d879442375f6ebebeca
Milagrosbriano <milagrosbriano@gmail.com>
2023-05-10 18:38:32 -0300

    grasp and solid 3

--- src/Library/AllInOnePrinter.cs
+++ src/Library/AllInOnePrinter.cs
@@ -4,28 +4,14 @@
 // </copyright>
 //---------------------------------------------------------------------------------
 using System;
-using System.IO;
 
 namespace Full_GRASP_And_SOLID.Library
 {
-    public enum Destination
+    public class ConsolePrinter : IPrinter
     {
-        Console,
-        File
-    }
-
-    public class AllInOnePrinter
-    {
-        public void PrintRecipe(Recipe recipe, Destination destination)
+        public void PrintTicket(Recipe recipe)
         {
-            if (destination == Destination.Console)
-            {
-                Console.WriteLine(recipe.GetTextToPrint());
-            }
-            else
-            {
-                File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
-            }
+            Console.WriteLine(recipe.GetTextToPrint());
         }
     }
 }
